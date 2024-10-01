8e4e42e25280a47c87e698548e44fb33d5b7dd7f
nadhyap <nadhyap@amazon.com>
2024-10-01 18:52:33 +0200

    addressing relevant cdk_nag errors

--- sample.py
+++
@@ -1,7 +0,0 @@
-def process_education_column(df):
-    print(f"Type of df: {type(df)}")
-
-    # Create a new column based on whether 'high' is in the 'education' column
-    df = df.str.contains('high', case=False, na=False).map({True: 'Yes', False: 'No'})
-    
-    return df
